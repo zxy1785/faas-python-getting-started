225141843cf6b8568d89e39572e1441265238312
Cristi Miloiu <65810476+cristim67@users.noreply.github.com>
2024-10-16 11:58:12 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 def handler(event):
     print("Function was invoked with event: {}".format(event))
+    
+    name = event.get("queryStringParameters", {}).get("name", "World")
+    
     return {
         "statusCode": 200,
-        "body": "Hello, World!"
+        "body": f"Hello, {name}! Welcome to Genezio Functions!"
     }
